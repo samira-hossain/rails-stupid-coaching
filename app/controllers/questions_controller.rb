ae96cbcf7bdf04a5dbd80cb7296ecb6feb741148
samira hossain <samira.hossain@outlook.com>
2023-03-18 13:51:50 +1000

    Stupid Coaching Rails

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,19 @@
 class QuestionsController < ApplicationController
+
   def ask
-    if your_message == "I am goint to work"
-      return "Great!"
-    elsif your_message.end_with?("?")
-      return "Silly question, get dressed and go to work!"
-    else
-      "I don't care, get dressed and go to work!"
-    end
+  end
+
+  def answer
+    # get the question from params
+    @question = params[:question]
+    # determine the answer based on question
+    if @question == "I am goint to work"
+      @answer = "Great!"
+     elsif @question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+     else
+      @answer = "I don't care, get dressed and go to work!"
+     end
+    # display the answer on the view
   end
 end
